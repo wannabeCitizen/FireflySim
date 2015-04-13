e137c5f8cefd10ba16f10ea1b53d05294a2c4caf
wannabeCitizen <doorsofskirpan@gmail.com>
2015-04-12 22:38:37 -0600

    Added Python Code

---
+++ controller.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import time
+import sys
+
+from neopixel import *
+import ff_sim
+
+# ColorRGB = (255, 100, 0)
+PINS = [18, 27, 23, 25]
+
+
+
+def runner(per_strip, strip, my_flies, strip_handle):
+    #update state of all ffs
+    my_flies1 = update_state(my_flies, .001, strip, per_strip)
+
+    #check who blinked
+    for i in xrange(strip):
+        for j in xrange(per_strip):
+            strip_handle[i].setPixelColorRGB(j, int(my_flies[i][j].brightnessR), int(my_flies[i][j].brightnessG))
+
+    #update strip
+    for i in xrange(strip):
+        strip[i].show()
+
+    return my_flies1
+
+
+#Expects to run with 2 arguments:
+#num of strips and num of ff's per strip
+if __name__ == '__main__':
+    #Grab input arguments
+    ff_num = int(raw_input("How many fireflies are on a strip?:  "))
+    strip_num = int(raw_input("How many strips?:  "))
+    stim_w = float(raw_input("Stimulus Period?:  "))
+    A_min = float(raw_input("Minimum Reset Strength A?:  "))
+    A_max = float(raw_input("Maximum Reset Strength A?:  "))
+    w_min = float(raw_input("Minimum Period:  "))
+    w_max = float(raw_input("Maximum Period?:  "))
+
+    try: 
+        PINS[strip_num - 1]
+    except IndexError:
+        print "You're trying to use too many strips given the set pins"
+        print "Consider changing the pin settings or chaining your strips\n\n"
+
+
+    #Create FF Agents
+    FFs = make_ff_array(ff_num, strip_num, A_max, A_min, stim_w, w_max, w_min)
+
+    #Create Strip Objects
+    strips = []
+    for i in range(strip_num):
+        next_strip = Adafruit_NeoPixel(ff_num, PINS[i])
+        strips.append(next_strip)
+        next_strip.begin()
+
+
+    #Start Running
+    print "Press Ctrl-C to stop at any time"
+    while True:
+        FFs = runner(ff_num, strip_num, FFs, strips)
+        time.sleep(.001)
+
+
+
+
+    
---
+++ ff_sim.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import numpy as np
+
+
+class FireFly:
+    
+    # Firefly period is set in seconds
+    def __init__(self, T, A):
+        self.T = T
+        self.w0 = ((2*np.pi)/T)
+        self.wn = ((2*np.pi)/T)
+        self.A = A
+        self.theta = 0
+        self.theta1 = -1
+        self.blink = 0
+        self.brightnessR = 0
+        self.brightnessG = 0
+
+    def next_state(self, t):
+        self.theta1 = self.theta + self.wn * t
+        if (np.sin(self.theta) < 0) and (np.sin(self.theta1) >= 0):
+            self.blink = 1
+            self.brightness = 1
+        self.theta = self.theta1
+        if self.blink == 1:
+            self.brightnessR += .85
+            self.brightnessG += .33
+            if round(self.brightnessR) == 255:
+                self.blink = 0
+        elif (not self.blink) and (self.brightness > 0):
+            self.brightnessR -= .51
+            self.brightnessG -= .2
+
+    def update(self, theta_stim):
+        self.wn = self.wn + (self.A * np.sin(theta_stim - self.theta1))
+
+#Expects number of FF's to evenly divide into strips
+#This is because this is meant to work with LED or other lighting strips
+#Thus, if you ask for more FF's than LEDs, we need to pitch a few (or do unnecessary computational work)
+def make_ff_array(strip_length, num_strips, A_max, A_min, w_stim, w_max, w_min):
+    ff_arrays = {}
+
+    ff_arrays['stim'] = Firefly(w_stim, A_min)
+    for i in xrange(num_strips):
+        ff_strip = []
+        for j in range(strip_length):
+            _w = (np.random.random() * (w_max - w_min)) + w_min
+            _A = (np.random.random() * (A_max - A_min)) + A_min
+
+            next_ff = FireFly(_w, _A)
+            arr.push(next_ff)
+        ff_arrays[i] = arr
+    return ff_arrays
+
+def update_state(ff_array, t, num_strips, strip_length):
+    phase1 = ff_array['stim'].theta
+    ff_array['stim'].next_state(t)
+    phase2 = ff_array['stim'].theta1
+    for i in xrange(num_strips):
+        for j in xrange(strip_length):
+            ff_array[i][j].next_state(t)
+    if (np.sin(phase1) < 0) and (np.sin(phase2) >= 0):
+        for i in xrange(num_strips):
+            for j in xrange(strip_length):
+                ff_array[i][j].update(phase2)
+    return ff_array
